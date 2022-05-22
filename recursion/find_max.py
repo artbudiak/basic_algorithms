acaf820bdd28a3b3bfb97c0862ff9cc3a3940833
Artem Budiak <art.budiak@gmail.com>
2022-05-22 17:33:15 +0300

    find max and sum arr

---
+++ recursion/find_max.py
@@ -0,0 +1,8 @@
+def find_max(arr):
+    if len(arr) == 2:
+        return arr[0] if arr[0] > arr[1] else arr[1]
+    sub_max = find_max(arr[1:])
+    return arr[0] if arr[0] > sub_max else sub_max
+
+list = [300, 1, 20, 3, 41, 201]
+print(find_max(list))
